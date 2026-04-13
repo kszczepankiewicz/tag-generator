d0015fa2ce30e766c56b622e755045dd20acdb89
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-04-13 22:51:29 +0200

    feat: add text and reset

--- script.js
+++ script.js
@@ -7,13 +7,19 @@ class Tag {
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 const pre = document.querySelector('pre');
+const reset = document.getElementById('reset');
 
 let tag;
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     if (!tag) tag = new Tag(input.value);
-    if (!tag.text) tag.text = input.value;
+    else if (!tag.text) tag.text = input.value;
     pre.textContent = `<${tag.tag}>${tag.text || ''}</${tag.tag}>`;
     input.value = '';
+});
+
+reset.addEventListener('click', (e) => {
+    tag = undefined;
+    pre.textContent = '';
 });
