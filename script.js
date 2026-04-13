b512e76d91f84b5ee86688b2c3515112ce02c920
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-04-13 22:36:33 +0200

    feat: add tag

---
+++ script.js
@@ -0,0 +1,18 @@
+class Tag {
+    constructor(tag) {
+        this.tag = tag;
+    }
+}
+
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const pre = document.querySelector('pre');
+
+let tag;
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (!tag) tag = new Tag(input.value);
+    pre.textContent = `<${tag.tag}>${tag.text || ''}</${tag.tag}>`;
+    input.value = '';
+});
